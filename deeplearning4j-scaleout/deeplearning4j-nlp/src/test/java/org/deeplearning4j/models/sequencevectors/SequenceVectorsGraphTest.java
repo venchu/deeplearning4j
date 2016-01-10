4bd6eddb7156644c707046752aadd6decc24b783
raver119 <raver119@gmail.com>
2016-01-10 18:23:53 +0300

    initial commit

---
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/sequencevectors/SequenceVectorsGraphTest.java
@@ -0,0 +1,34 @@
+package org.deeplearning4j.models.sequencevectors;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is going to be basic for SequenceVectors graph transformation
+ *
+ * @author raver119@gmail.com
+ */
+public class SequenceVectorsGraphTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    /**
+     * In this test/example we'll assume that we have csv file that contains transactions sorted by <source asc, time asc>
+     * @throws Exception
+     */
+    @Test
+    public void testFit() throws Exception {
+
+    }
+}
---
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/sequencevectors/classes/Transaction.java
@@ -0,0 +1,26 @@
+package org.deeplearning4j.models.sequencevectors.classes;
+
+import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
+
+/**
+ * @author raver119@gmail.com
+ */
+public class Transaction extends SequenceElement {
+    /**
+     * This method should return string representation of this SequenceElement, so it can be used for
+     *
+     * @return
+     */
+    @Override
+    public String getLabel() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String toJSON() {
+        return null;
+    }
+}
