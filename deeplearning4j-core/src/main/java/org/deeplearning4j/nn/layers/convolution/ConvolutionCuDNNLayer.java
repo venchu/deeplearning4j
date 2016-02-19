84edec89a0538c5c06c616efe92f9841ef4a2d2e
nyghtowl <warrick.melanie@gmail.com>
2016-02-19 23:29:43 +1100

    Exploring jcudnn. Pulling in ideas for framework but tbd on whether this will be covered elsewhere to keep dl4j background independent.

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/convolution/ConvolutionCuDNNLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/convolution/ConvolutionCuDNNLayer.java
@@ -32,14 +32,14 @@ import org.nd4j.linalg.api.ops.BroadcastOp;
 import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastAddOp;
 import org.nd4j.linalg.convolution.Convolution;
 import org.nd4j.linalg.factory.Nd4j;
+import static jcuda.jcublas.JCublas2.cublasCreate;
 
 import java.util.Arrays;
 
 
 /**
- * Convolution layer
+ * Convolution layer integrated to CuDNN
  *
- * @author Adam Gibson
  */
 public class ConvolutionCuDNNLayer extends BaseLayer<org.deeplearning4j.nn.conf.layers.ConvolutionLayer> {
     protected INDArray col; // vectorized input
@@ -53,10 +53,9 @@ public class ConvolutionCuDNNLayer extends BaseLayer<org.deeplearning4j.nn.conf.
     }
 
 
-    public void setCol(INDArray col) {this.col = col;}
-
     @Override
     public double calcL2() {
+        // TODO keep params on gpu and pass in l2 norm to apply
     	if(!conf.isUseRegularization() || conf.getLayer().getL2() <= 0.0 ) return 0.0;
 
         double l2Norm = getParam(ConvolutionParamInitializer.WEIGHT_KEY).norm2Number().doubleValue();
@@ -65,14 +64,12 @@ public class ConvolutionCuDNNLayer extends BaseLayer<org.deeplearning4j.nn.conf.
 
     @Override
     public double calcL1() {
+        // TODO keep params on gpu and pass in l1 norm to apply
+
     	if(!conf.isUseRegularization() || conf.getLayer().getL1() <= 0.0 ) return 0.0;
         return conf.getLayer().getL1() * getParam(ConvolutionParamInitializer.WEIGHT_KEY).norm1Number().doubleValue();
     }
 
-    @Override
-    public Type type() {
-        return Type.CONVOLUTIONAL;
-    }
 
     public INDArray calculateDelta(INDArray epsilon) {
         INDArray z = preOutput(true);
@@ -81,6 +78,18 @@ public class ConvolutionCuDNNLayer extends BaseLayer<org.deeplearning4j.nn.conf.
             throw new IllegalStateException("Shapes must be same");
         return epsilon.muli(activationDerivative);
 
+    }
+
+    void addBias(cudnnTensorDescriptor dstTensorDesc,
+                 Layer layer, int c, Pointer data)
+    {
+        cudnnSetTensor4dDescriptor(biasTensorDesc,
+                CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
+                1, c, 1, 1);
+        Pointer alpha = pointerTo(1.0f);
+        Pointer beta = pointerTo(1.0f);
+        cudnnAddTensor(cudnnHandle, CUDNN_ADD_SAME_C, alpha,
+                biasTensorDesc, layer.bias_d, beta, dstTensorDesc, data);
     }
 
     @Override
@@ -138,23 +147,128 @@ public class ConvolutionCuDNNLayer extends BaseLayer<org.deeplearning4j.nn.conf.
         return activation;
     }
 
-    @Override
-    public Layer transpose(){
-        throw new UnsupportedOperationException("Not yet implemented");
+    void convoluteForward(Layer conv, TensorLayout t,
+                          Pointer srcData, Pointer dstData)
+    {
+        int algo = 0; // cudnnConvolutionFwdAlgo_t
+
+        cudnnSetTensor4dDescriptor(srcTensorDesc,
+                CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
+                t.n, t.c, t.h, t.w);
+
+        int tensorDims = 4;
+        int tensorOuputDimA[] = { t.n, t.c, t.h, t.w };
+        int filterDimA[] = {
+                conv.outputs, conv.inputs,
+                conv.kernel_dim, conv.kernel_dim };
+        cudnnSetFilterNdDescriptor(filterDesc,
+                CUDNN_DATA_FLOAT, tensorDims, filterDimA);
+
+        int convDims = 2;
+        int padA[] = { 0, 0 };
+        int filterStrideA[] = { 1, 1 };
+        int upscaleA[] = { 1, 1 };
+        cudnnSetConvolutionNdDescriptor(convDesc, convDims, padA,
+                filterStrideA, upscaleA, CUDNN_CROSS_CORRELATION);
+
+        // find dimension of convolution output
+        cudnnGetConvolutionNdForwardOutputDim(convDesc,
+                srcTensorDesc, filterDesc,
+                tensorDims, tensorOuputDimA);
+        t.n = tensorOuputDimA[0];
+        t.c = tensorOuputDimA[1];
+        t.h = tensorOuputDimA[2];
+        t.w = tensorOuputDimA[3];
+
+        cudnnSetTensor4dDescriptor(dstTensorDesc,
+                CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
+                t.n, t.c, t.h, t.w);
+
+        if (convAlgorithm < 0)
+        {
+            int algoArray[] = { -1 };
+
+            // Choose the best according to the preference
+            System.out.println(
+                    "Testing cudnnGetConvolutionForwardAlgorithm ...");
+            cudnnGetConvolutionForwardAlgorithm(cudnnHandle, srcTensorDesc,
+                    filterDesc, convDesc, dstTensorDesc,
+                    CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 0, algoArray);
+            algo = algoArray[0];
+
+            System.out.println("Fastest algorithm is Algo " + algo);
+            convAlgorithm = algo;
+
+            // New way of finding the fastest config
+            // Setup for findFastest call
+            System.out.println(
+                    "Testing cudnnFindConvolutionForwardAlgorithm ...");
+            int requestedAlgoCount = 5;
+            int returnedAlgoCount[] = new int[1];
+            cudnnConvolutionFwdAlgoPerf results[] =
+                    new cudnnConvolutionFwdAlgoPerf[requestedAlgoCount];
+            cudnnFindConvolutionForwardAlgorithm(cudnnHandle,
+                    srcTensorDesc, filterDesc, convDesc, dstTensorDesc,
+                    requestedAlgoCount, returnedAlgoCount, results);
+            for (int algoIndex = 0; algoIndex < returnedAlgoCount[0]; ++algoIndex)
+            {
+                System.out.printf(
+                        "    %s for Algo %d (%s): %f time requiring %d memory\n",
+                        cudnnGetErrorString(results[algoIndex].status),
+                        results[algoIndex].algo,
+                        cudnnConvolutionFwdAlgo.stringFor(results[algoIndex].algo),
+                        results[algoIndex].time, results[algoIndex].memory);
+            }
+        }
+        else
+        {
+            algo = convAlgorithm;
+            if (algo == CUDNN_CONVOLUTION_FWD_ALGO_FFT)
+            {
+                System.out.println("Using FFT for convolution");
+            }
+        }
+
+        resize(t.n * t.c * t.h * t.w, dstData);
+        long sizeInBytesArray[] = { 0 };
+        Pointer workSpace = new Pointer();
+        cudnnGetConvolutionForwardWorkspaceSize(cudnnHandle,
+                srcTensorDesc, filterDesc, convDesc, dstTensorDesc,
+                algo, sizeInBytesArray);
+        long sizeInBytes = sizeInBytesArray[0];
+        if (sizeInBytes != 0)
+        {
+            cudaMalloc(workSpace, sizeInBytes);
+        }
+
+        Pointer alpha = pointerTo(1.0f);
+        Pointer beta = pointerTo(0.0f);
+        cudnnConvolutionForward(cudnnHandle, alpha, srcTensorDesc,
+                srcData, filterDesc, conv.data_d, convDesc, algo,
+                workSpace, sizeInBytes, beta, dstTensorDesc, dstData);
+        addBias(dstTensorDesc, conv, t.c, dstData);
+        if (sizeInBytes != 0)
+        {
+            cudaFree(workSpace);
+        }
     }
+    void activationForward(TensorLayout t,
+                           Pointer srcData, Pointer dstData)
+    {
+        resize(t.n * t.c * t.h * t.w, dstData);
 
+        cudnnSetTensor4dDescriptor(srcTensorDesc,
+                CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
+                t.n, t.c, t.h, t.w);
+        cudnnSetTensor4dDescriptor(dstTensorDesc,
+                CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT,
+                t.n, t.c, t.h, t.w);
 
-    @Override
-    public Gradient calcGradient(Gradient layerError, INDArray indArray) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public void fit(INDArray input) {}
-
-    @Override
-    public void merge(Layer layer, int batchSize) {
-        throw new UnsupportedOperationException();
+        Pointer alpha = pointerTo(1.0f);
+        Pointer beta = pointerTo(0.0f);
+        cudnnActivationForward(cudnnHandle, CUDNN_ACTIVATION_RELU,
+                alpha, srcTensorDesc, srcData,
+                beta, dstTensorDesc, dstData);
     }
 
 
